e94641225760764d5e35ca1240555da6d66fc65b
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-05-22 15:52:01 +0900

    Back to Normal

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,7 +10,7 @@ env :PATH, ENV['PATH']
 # ログファイルの出力先
 set :output, 'log/cron.log'
 # ジョブの実行環境の指定 あとで開発に直す！
-set :environment, :production
+set :environment, :development
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -21,8 +21,8 @@ set :environment, :production
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
-every 1.minutes do ##テストのためここを追加
-# every 1.days, at: '9:00 am' do
+# every 1.minutes do ##テストのためここを追加
+every 1.days, at: '9:00 am' do
 
 # Rails内のメソッド実行
   runner "ThanksMailer.notify_user"
